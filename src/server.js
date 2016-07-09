22e6f82d61d42bb71b48417b0f85b4671399164a
Michael Gilliland <mjg.py3@gmail.com>
2016-07-09 09:08:12 -0400

    Add POST route that saves data to a file

--- src/server.js
+++ src/server.js
@@ -1,10 +1,29 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+var uuid = require('node-uuid');
+
+app.use(require('body-parser').json());
 
 app.get('/', (_, res) => {
   console.log('got a request (down here in Node-land)');
 
   res.send('Hello World!');
+});
+
+app.post('/file', (req, res) => {
+  console.log('POST /file :)');
+  console.log('CONTENT:', req.body);
+
+  const filename = `${uuid.v4()}.json`;
+
+  fs.writeFile(`/tmp2/${filename}`, JSON.stringify(req.body), (err) => {
+    if (err) {
+      return res.status(500).send('failed to save content');
+    }
+
+    res.send(`The file was saved as ${filename}`);
+  });
 });
 
 app.listen(3000, () => {
